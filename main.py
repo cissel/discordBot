c1c86ea9dec22414551aa2061e3bb721c18e7138
cissel <jamescissel@gmail.com>
2025-03-24 16:07:06 -0400

    Added online/offline messages to test env channel

--- main.py
+++ main.py
@@ -2,12 +2,25 @@ import discord
 import subprocess
 from dotenv import load_dotenv
 import os
+import asyncio
+import signal
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+TEST_ENV_CHANNEL_ID = int(os.getenv("TEST_ENV_CHANNEL_ID"))
+
+intents = discord.Intents.default()
+intents.message_content = True
 
 class Client(discord.Client):
 
     async def on_ready(self):
 
-        print(f'{self.user} online!')
+        print(f'online!')
+
+        channel = client.get_channel(TEST_ENV_CHANNEL_ID)
+        if channel:
+            await channel.send(f'going online!')
 
     async def on_message(self, message):
 
@@ -42,11 +55,33 @@ class Client(discord.Client):
             subprocess.run(["Rscript", "flRada.R"])
             await message.channel.send("here's the latest radar loop:", file=discord.File("flRadar.gif"))
 
-load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
+    async def send_goodbye_message(self):
 
-intents = discord.Intents.default()
-intents.message_content = True
+        """Sends a shutdown message before the bot exits."""
+        channel = self.get_channel(TEST_ENV_CHANNEL_ID)
+        if channel:
+            print("Sending goodbye message")
+            await channel.send(f'going offline!')
+            await asyncio.sleep(2)
+        else:
+            print(f"Could not find channel {TEST_ENV_CHANNEL_ID}")
 
 client = Client(intents=intents)
+
+async def shutdown_handler():
+    """Handles cleanup when the bot is shutting down."""
+    print("Shutting down bot...")
+    await client.send_goodbye_message()  # Send the goodbye message
+    await client.close()  # Properly disconnect the bot
+
+# Handle Ctrl+C manually to trigger the shutdown message
+loop = asyncio.get_event_loop()
+
+try:
+    loop.run_until_complete(client.start(TOKEN))
+except KeyboardInterrupt:
+    loop.run_until_complete(shutdown_handler())
+finally:
+    loop.close()
+
 client.run(TOKEN)
